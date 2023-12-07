f8c6a7abdaad59b3f57c7794f856050670fda5bf
Randall (Yichen) Liao <36297267+liaoyichen123@users.noreply.github.com>
2023-12-07 15:04:04 -0500

    modify DDLs in data loder

--- db_data_loader.py
+++ db_data_loader.py
@@ -23,41 +23,96 @@ conn.execute("DROP TABLE IF EXISTS locations")
 conn.execute("DROP TABLE IF EXISTS raw_data")
 
 # Create tables
-conn.execute('''CREATE TABLE count_metadata (
-                _id INTEGER,
-                count_id INTEGER,
-                count_date TEXT,
-                location_id INTEGER,
-                location TEXT,
-                lng REAL,
-                lat REAL,
-                centreline_type REAL,
-                centreline_id REAL,
-                px REAL)''')
+conn.execute('''create table count_metadata
+(
+    _id             INTEGER,
+    count_id        INTEGER,
+    count_date      TEXT,
+    location_id     INTEGER,
+    location        TEXT,
+    lng             REAL,
+    lat             REAL,
+    centreline_type REAL,
+    centreline_id   REAL,
+    px              REAL
+)''')
 
-conn.execute('''CREATE TABLE locations (
-                _id INTEGER,
-                location_id INTEGER,
-                location TEXT,
-                lng REAL,
-                lat REAL,
-                centreline_type REAL,
-                centreline_id REAL,
-                px REAL,
-                latest_count_date TEXT)''')
+conn.execute('''create table locations
+(
+    _id               INTEGER,
+    location_id       INTEGER,
+    location          TEXT,
+    lng               REAL,
+    lat               REAL,
+    centreline_type   REAL,
+    centreline_id     REAL,
+    px                REAL,
+    latest_count_date TEXT
+)''')
 
 # Assuming raw_data has many columns, define them accordingly
-conn.execute('''CREATE TABLE raw_data (
-                _id INTEGER,
-                count_id INTEGER,
-                count_date TEXT,
-                location_id INTEGER,
-                location TEXT,
-                lng REAL,
-                lat REAL,
-                some_integer_column INTEGER,
-                some_text_column TEXT,
-                some_real_column REAL
+conn.execute('''create table raw_data
+(
+    _id             INTEGER,
+    count_id        INTEGER,
+    count_date      TEXT,
+    location_id     INTEGER,
+    location        TEXT,
+    lng             REAL,
+    lat             REAL,
+    centreline_type INTEGER,
+    centreline_id   INTEGER,
+    px              REAL,
+    time_start      TEXT,
+    time_end        TEXT,
+    sb_cars_r       INTEGER,
+    sb_cars_t       INTEGER,
+    sb_cars_l       INTEGER,
+    nb_cars_r       INTEGER,
+    nb_cars_t       INTEGER,
+    nb_cars_l       INTEGER,
+    wb_cars_r       INTEGER,
+    wb_cars_t       INTEGER,
+    wb_cars_l       INTEGER,
+    eb_cars_r       INTEGER,
+    eb_cars_t       INTEGER,
+    eb_cars_l       INTEGER,
+    sb_truck_r      INTEGER,
+    sb_truck_t      INTEGER,
+    sb_truck_l      INTEGER,
+    nb_truck_r      INTEGER,
+    nb_truck_t      INTEGER,
+    nb_truck_l      INTEGER,
+    wb_truck_r      INTEGER,
+    wb_truck_t      INTEGER,
+    wb_truck_l      INTEGER,
+    eb_truck_r      INTEGER,
+    eb_truck_t      INTEGER,
+    eb_truck_l      INTEGER,
+    sb_bus_r        INTEGER,
+    sb_bus_t        INTEGER,
+    sb_bus_l        INTEGER,
+    nb_bus_r        INTEGER,
+    nb_bus_t        INTEGER,
+    nb_bus_l        INTEGER,
+    wb_bus_r        INTEGER,
+    wb_bus_t        INTEGER,
+    wb_bus_l        INTEGER,
+    eb_bus_r        INTEGER,
+    eb_bus_t        INTEGER,
+    eb_bus_l        INTEGER,
+    nx_peds         INTEGER,
+    sx_peds         INTEGER,
+    ex_peds         INTEGER,
+    wx_peds         INTEGER,
+    nx_bike         INTEGER,
+    sx_bike         INTEGER,
+    ex_bike         INTEGER,
+    wx_bike         INTEGER,
+    nx_other        INTEGER,
+    sx_other        INTEGER,
+    ex_other        INTEGER,
+    wx_other        INTEGER
                 )''')
 
 
